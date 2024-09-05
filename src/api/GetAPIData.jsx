40a7f1b68e8e45ef28cd03a509303722f49958fd
Yashraj950 <yashkumar9ipo@gmail.com>
2024-09-05 20:20:50 +0530

    good

--- src/api/GetAPIData.jsx
+++ src/api/GetAPIData.jsx
@@ -1,10 +1,10 @@
 export const getMoviesData = async () => {
     try {
       const response = await fetch(
-        https://www.omdbapi.com/?i=tt3896198&apikey=${
-          import.meta.env.VITE_API_KEY
-        }&s=titanic&page=1
-      );
+        "http://www.omdbapi.com/?i=tt3896198&apikey=1a0d5307"
+      )
+
+     
       const data = response.json();
       return data;
     } catch (error) {
