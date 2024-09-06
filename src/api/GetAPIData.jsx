e6b6805b4cde1cac48b0cfaf81914a543a835598
Yashraj950 <yashkumar9ipo@gmail.com>
2024-09-06 09:36:08 +0530

    update Api

--- src/api/GetAPIData.jsx
+++ src/api/GetAPIData.jsx
@@ -1,14 +1,11 @@
 export const getMoviesData = async () => {
-    try {
-      const response = await fetch(
-        "http://www.omdbapi.com/?i=tt3896198&apikey=1a0d5307"
-      )
-
-     
-      const data = response.json();
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
+  try {
+    const response = await fetch(
+         "  http://www.omdbapi.com/?i=tt3896198&apikey=d51acbb9&s=titanic&page=1"
+    );
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,18 +1,18 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData } from "react-router-dom";
+import { Card } from "../components/UI/Card";
 
 export const Movie = () => {
-    const moviesData = useLoaderData();
-    console.log(moviesData);
-    
-    
-    
-    
-    return ( <>
-   { moviesData.search.map((curMovie) => {
-    return <Card key = {curMovie.imdbID} curMovie={curMovie}/>;
-   })}
-   </>
-   
-)
-    
-}
+  const moviesData = useLoaderData();
+  console.log(moviesData);
+
+  return (
+    <>
+      <ul className="container grid gird-four--cols">
+        {moviesData &&
+        moviesData.search.map((curMovie) => {
+          return <Card key={curMovie.imdbID} curMovie={curMovie} />;
+        })}
+      </ul>
+    </>
+  );
+};
